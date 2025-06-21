e57d8377bd4a87fb79d4c22cc5b013ebf82dfef6
MangoCode0 <malik.asgar@outlook.com>
2025-06-21 22:29:22 +0530

    add something new

---
+++ doubly_linked_list.c
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Node {
+    int data;
+    struct Node* next;
+    struct Node* prev;
+};
+
+struct Node* createNode(int data) {
+    struct Node* newNode = (struct Node*)malloc(sizeof(struct Node));
+    newNode->data = data;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+void insertAtBeginning(struct Node** head_ref, int new_data) {
+    struct Node* newNode = createNode(new_data);
+    newNode->next = *head_ref;
+
+    if (*head_ref != NULL) {
+        (*head_ref)->prev = newNode;
+    }
+
+    *head_ref = newNode;
+}
+
+void displayList(struct Node* node) {
+    struct Node* last;
+    printf("Traversal in forward direction:\n");
+    while (node != NULL) {
+        printf("%d ", node->data);
+        last = node;
+        node = node->next;
+    }
+    printf("\nTraversal in reverse direction:\n");
+    while (last != NULL) {
+        printf("%d ", last->data);
+        last = last->prev;
+    }
+    printf("\n");
+}
+
+int main() {
+    struct Node* head = NULL;
+
+    insertAtBeginning(&head, 10);
+    insertAtBeginning(&head, 20);
+    insertAtBeginning(&head, 30);
+
+    displayList(head);
+
+    return 0;
+}
