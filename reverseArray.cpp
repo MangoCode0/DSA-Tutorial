3078504c4e03e5d45f3269ae2bae14f4e165a367
MangoCode0 <malik.asgar@outlook.com>
2025-06-25 07:36:58 +0530

    reverse array prob

--- reverseArray.cpp
+++ reverseArray.cpp
@@ -12,7 +12,6 @@ void reverseArray(int arr[], int start, int end) {
 int main() {
     int arr[] = {1, 2, 3, 4, 5};
     int n = sizeof(arr) / sizeof(arr[0]);
-
     reverseArray(arr, 0, n - 1);
 
     for (int i = 0; i < n; i++) {
