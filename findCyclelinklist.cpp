36c4e5685d08817914cb1c744d66d797a31d9e11
MangoCode0 <malik.asgar@outlook.com>
2025-07-11 23:12:51 +0530

    .\findCyclelinklist.cpp

---
+++ findCyclelinklist.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+struct Node {
+    int data;
+    Node* next;
+};
+
+bool hasCycle(Node* head) {
+    if (head == nullptr || head->next == nullptr) return false;
+
+    Node* slow = head;
+    Node* fast = head->next;
+
+    while (fast != nullptr && fast->next != nullptr) {
+        if (slow == fast) return true;
+        slow = slow->next;
+        fast = fast->next->next;
+    }
+    return false;
+}
+
+int main() {
+    Node* head = new Node{1, nullptr};
+    head->next = new Node{2, nullptr};
+    head->next->next = new Node{3, nullptr};
+    head->next->next->next = head->next;  // Create a cycle
+
+    if (hasCycle(head)) {
+        cout << "Cycle detected" << endl;
+    } else {
+        cout << "No cycle detected" << endl;
+    }
+
+    return 0;
+}
